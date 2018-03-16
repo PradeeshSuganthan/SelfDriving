7d7dbb25de4a4bfcbb837b19fd2608528f841eea
Pradeesh Suganthan <pradeesh.suganthan@gmail.com>
2018-03-15 22:55:24 -0700

    Linear Kalman Filter works

--- SLAM/kalmanFilters/KalmanFilter.py
+++ SLAM/kalmanFilters/KalmanFilter.py
@@ -8,49 +8,75 @@ arrayshape = (timesteps, )
 
 #create synthetic data
 pure = np.linspace(0,10,timesteps)
-
 noise = np.random.normal(0,1,timesteps)
 
 signal = pure + noise
 
 plt.scatter(range(100), signal)
 plt.title("Synthetic data")
-plt.show()
 
 
 
 #create filter
+#state vector (estimate)
+x = np.zeros(arrayshape)
+xpred = np.zeros(arrayshape)
+
+#covariance matrix (unceratainty)
+P = np.zeros(arrayshape)
+
+#State transition model
+F = 1
+#control input
+B = 0
+#control vector
+u = 0
+#Noise covariance
+Q = 1
+#Measurement covariance
+R = 1
+#Sensor model
+H = 1
+  
+
+#prediction 
+def predict(x,P,t):
+    #State prediction
+    x_pred = F*x[t-1] + B*u
+    #Covariance prediciton
+    P_pred = F*P[t-1]*F + Q
+
+    return x_pred, P_pred
+
 #measurement update
-def update(x,mean2,P,var2):
-#    x = 
-#    P = 
+def update(x_pred,P_pred,z,t):
+    #Innovation
+    y = z-H*x_pred
+    #Innovation Covariance
+    S = H*P_pred*H + R
+    #Kalman Gain
+    K = (P_pred*H)/S
+    #State update
+    x = x_pred + K*y
+    #Covariance Update
+    P = P_pred - K*(H*P_pred)
+
 
     return x, P
 
-#prediction update
-def predict(x,mean2,P,var2):
-#    x[t] = 
-#    P[t] = 
-
-    return x, P
 
 
-
-motion = np.diff(signal)
-
-mean = 0
-variance = 10000
-measurement_sig = 4
-motion_sig = 2
-
-for i in range(timesteps-1):
-    mean, variance = update(mean, signal[i],variance, measurement_sig)
-    print('Update: %d, %d' % (mean, variance))
-
-    mean, variance = predict(mean, motion[i], variance, motion_sig )
-    print('Predict: %d, %d'% (mean, variance))
-
+for i in range(1, timesteps):
+    #predict
+    x_pred, P_pred = predict(x, P,i)
+    xpred[i] = x_pred
+    #update
+    x[i], P[i] = update(x_pred,P_pred,signal[i],i)
 
 
 
 #visualize
+plt.plot(xpred, color='red')
+plt.show()
+
+
